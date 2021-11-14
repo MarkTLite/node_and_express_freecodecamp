c48b96c139ce1a2842d31353dd8aafac178e33a3
MarkTLite <profturyahabwa@gmail.com>
2021-11-14 16:56:03 +0300

    Setup MongoDB

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@
 FROM node:12.18.1
 ENV NODE_ENV=production
 
+
 WORKDIR /app
 
 COPY ["package.json", "package-lock.json*", "./"]
--- myApp.js
+++ myApp.js
@@ -8,7 +8,7 @@ var dotenv = require('dotenv') ;
 dotenv.config();
 
 //after setting up a hosted database on MongoDB Atlas, connect to the database
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://Mark_T:<test12345>@cluster0-node-and-expre.nwvxj.mongodb.net/Cluster0-node-and-express-freecodecamp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 
