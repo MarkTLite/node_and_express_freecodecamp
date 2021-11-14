8198c8844b4fffa823360b2f8a8d54d65bddd9af
MarkTLite <profturyahabwa@gmail.com>
2021-11-14 15:42:47 +0300

    set up and install  MongoDB

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+//Add mongodb@~3.6.0 and mongoose@~5.4.0 to the project’s package.json. require mongoose as mongoose in myApp.js.
+var mongoose = require(mongoose);
+
+//after setting up a hosted database on MongoDB Atlas, connect to the database
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 
 
 
